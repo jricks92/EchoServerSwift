0c94b7019ed657fdbf60e1f323908e48dde10c90
Jameson Ricks <jameson@glenricks.com>
2017-02-04 16:35:24 -0700

    Initial Commit

---
+++ Echo Server Swift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Echo Server Swift
+//
+//  Created by Jameson Ricks on 2/4/17.
+//  Copyright © 2017 Jameson Ricks. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
